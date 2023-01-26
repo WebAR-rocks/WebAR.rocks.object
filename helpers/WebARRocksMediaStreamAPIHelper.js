1930444a163a30c4bce73d1c5d5887d7ad81ef09
xavierjs <xavier.bourry@polytechnique.org>
2023-01-26 09:35:03 -0500

    [FIX] Wide camera was selected on Samsung S22

--- helpers/WebARRocksMediaStreamAPIHelper.js
+++ helpers/WebARRocksMediaStreamAPIHelper.js
@@ -32,11 +32,11 @@
     return isIOS;
   },
 
-  get_IOSVersion: function(){ // from https://stackoverflow.com/questions/8348139/detect-ios-version-less-than-5-with-javascript
+  /*get_IOSVersion: function(){ // from https://stackoverflow.com/questions/8348139/detect-ios-version-less-than-5-with-javascript
     // added on 2019-04-11 for a tweak for IOS>=12.2
     const v = (navigator['appVersion']).match(/OS (\d+)_(\d+)_?(\d+)?/);
     return (v && v.length && v.length > 2) ? [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)] : [0, 0, 0];
-  },
+  },*/
 
   is_portrait: function(){ // see https://stackoverflow.com/questions/4917664/detect-viewport-orientation-if-orientation-is-portrait-display-alert-message-ad
     try{
@@ -54,7 +54,7 @@
     return WebARRocksMediaStreamAPIHelper.check_isSafari() || WebARRocksMediaStreamAPIHelper.check_isIOS();
   },
 
-  check_isSafari: function(){ //https://stackoverflow.com/questions/7944460/detect-safari-browser
+  check_isSafari: function(){ // see https://stackoverflow.com/questions/7944460/detect-safari-browser
     const ua = navigator['userAgent'].toLowerCase(); 
     if (ua.indexOf('safari') !== -1) { 
       if (ua.indexOf('chrome') !== -1) {
@@ -282,25 +282,25 @@
   }, //end create_fallbackConstraints()
 
 
-  switch_whIfPortrait: function(mandatory){
+  /*switch_whIfPortrait: function(constraintsArg){
     if (WebARRocksMediaStreamAPIHelper.is_portrait()){ // portrait display
-      if (!mandatory || !mandatory['video']){
+      if (!constraintsArg || !constraintsArg['video']){
         return false;
       }
-      const mandatoryVideoWidth  = mandatory['video']['width'];
-      const mandatoryVideoHeight = mandatory['video']['height'];
-      if (!mandatoryVideoWidth || !mandatoryVideoHeight){
+      const constraintVideoWidth  = constraintsArg['video']['width'];
+      const constraintVideoHeight = constraintsArg['video']['height'];
+      if (!constraintVideoWidth || !constraintVideoHeight){
         return false;
       }
-      if (mandatoryVideoWidth['ideal'] && mandatoryVideoHeight['ideal']
-        && mandatoryVideoWidth['ideal']>mandatoryVideoHeight['ideal']){ // landscape resolution asked
-        mandatory['video']['height'] = mandatoryVideoWidth;
-        mandatory['video']['width']  = mandatoryVideoHeight;
+      if (constraintVideoWidth['ideal'] && constraintVideoHeight['ideal']
+        && constraintVideoWidth['ideal']>constraintVideoHeight['ideal']){ // landscape resolution asked
+        constraintsArg['video']['height'] = constraintVideoWidth;
+        constraintsArg['video']['width']  = constraintVideoHeight;
         return true;
       }
     } //end if is_portrait()
     return false;
-  },
+  },*/
 
 
   // ==== END CONSTRAINTS HANDLING ====
@@ -331,7 +331,7 @@
   }, //end mute()
 
 
-  toggle_stream( video, isPlaying, mandatoryConstraints){
+  toggle_stream( video, isPlaying, constraintsArg){
     if (video === null){
       return Promise.resolve();
     }
@@ -349,7 +349,7 @@
       const videoTrack = tracks[0];
       
       if (isPlaying){
-        WebARRocksMediaStreamAPIHelper.get(video, accept, reject, mandatoryConstraints);
+        WebARRocksMediaStreamAPIHelper.get(video, accept, reject, constraintsArg);
       } else {
         videoTrack['stop']();
         accept();
@@ -369,6 +369,7 @@
       isErrorRaw = true;
       callbackError(errCode);
     }
+
     const on_successRaw = function(video, localMediaStream, optionsReturned){
       if (isErrorRaw){
         console.log('WARNING in WebARRocksMediaStreamAPIHelper - get_raw(): cannot launch callbackSuccess because an error was thrown');
@@ -414,7 +415,7 @@
                 console.log('WARNING in WebARRocksMediaStreamAPIHelper - get_raw(): Image Capture API not found');
               }
 
-              if (WebARRocksMediaStreamAPIHelper.check_isApple()){
+              /*if (WebARRocksMediaStreamAPIHelper.check_isApple()){ // disabled on 2023-01-25
                 console.log('WARNING in WebARRocksMediaStreamAPIHelper - Apple device detected, add the video element to the DOM.');
 
                 if (!video['parentNode'] || video['parentNode']===null){
@@ -426,8 +427,8 @@
                   setTimeout(function(){
                     video['style']['transform'] = 'scale(0.0001,0.0001)';
                     video['style']['position'] = 'fixed';
-                    video['style']['bottom'] = '0px';
-                    video['style']['right'] = '0px';
+                    video['style']['bottom'] = '0';
+                    video['style']['right'] = '0';
                     WebARRocksMediaStreamAPIHelper.mute(video);
 
                     // from https://github.com/jeeliz/jeelizFaceFilter/issues/45
@@ -444,8 +445,9 @@
                   }, 100);
                 }
               } else { // not IOS:
+              */
                 on_successRaw(video, localMediaStream, optionsReturned);
-              }
+              //}
             } else { // video.currentTime is null
               on_errorRaw('VIDEO_NOTSTARTED');
               console.log('ERROR in callSuccessIfPlaying() - VIDEO_NOTSTARTED: video.currentTime =', video['currentTime']);
@@ -496,22 +498,22 @@
 
 
   /* Wrapper with promise for get method: */
-  get_promise: function(mandatory, videoArg){
+  get_promise: function(constraintsArg, videoArg){
     const video = videoArg || WebARRocksMediaStreamAPIHelper.get_videoElement();
     return new Promise(function(accept, reject){
-      WebARRocksMediaStreamAPIHelper.get(video, accept, reject, mandatory);
+      WebARRocksMediaStreamAPIHelper.get(video, accept, reject, constraintsArg);
     });
   },
 
 
   /* MAIN FUNCTION. ARGUMENTS:
-   * video: DOM video element (not jquery !!!)
+   * video: DOM video element
    * callback: function to launch if success, with video as argument
    * callbackError: function to launch if error happens
-   * mandatory: optionnal. mandatory constraints
+   * constraintsArg: optionnal. media constraints (both video and audio)
    */
    
-  get: function(video, callbackSuccess, callbackError, mandatory) {
+  get: function(video, callbackSuccess, callbackError, constraintsArg) {
     if (!video){
       console.log('ERROR in WebARRocksMediaStreamAPIHelper - get(): No video provided');
       if (callbackError){
@@ -521,16 +523,16 @@
     }
 
     if (!WebARRocksMediaStreamAPIHelper.compat()){
-      console.log('ERROR in WebARRocksMediaStreamAPIHelper - get(): No getUserMedia API found !');
+      console.log('ERROR in WebARRocksMediaStreamAPIHelper - get(): No getUserMedia API found!');
       if (callbackError){
         callbackError('MEDIASTREAMAPI_NOTFOUND');
       }
       return false;
     };
 
-    if (mandatory && mandatory['video']){
+    //if (constraintsArg && constraintsArg['video']){
 
-      if (WebARRocksMediaStreamAPIHelper.check_isIOS()){ // otherwise video freeze quickly
+      /*if (WebARRocksMediaStreamAPIHelper.check_isIOS()){ // otherwise video freeze quickly - disabled on 2023-01-25
         console.log('INFO in WebARRocksMediaStreamAPIHelper() - get(): iOS detected');
 
         // switch width and height video constraint if mobile portrait mode:
@@ -540,50 +542,106 @@
         // IOS is <12.2:
         if (iOSVersion[0] !== 0 && (iOSVersion[0]<12
           || (iOSVersion[0]===12 && iOSVersion[1]<2))){
-          WebARRocksMediaStreamAPIHelper.switch_whIfPortrait(mandatory);
+          WebARRocksMediaStreamAPIHelper.switch_whIfPortrait(constraintsArg);
         }
-      }
+      }*/
 
       // see https://stackoverflow.com/questions/45692526/ios-11-getusermedia-not-working:
       // disabled on 2022-07-20
-      /*if (mandatory['video']['width'] && mandatory['video']['width']['ideal']){
-        video['style']['width'] = mandatory['video']['width']['ideal'] + 'px';
+      /*if (constraintsArg['video']['width'] && constraintsArg['video']['width']['ideal']){
+        video['style']['width'] = constraintsArg['video']['width']['ideal'] + 'px';
       }
-      if (mandatory['video']['height'] && mandatory['video']['height']['ideal']){
-        video['style']['height'] = mandatory['video']['height']['ideal'] + 'px';
+      if (constraintsArg['video']['height'] && constraintsArg['video']['height']['ideal']){
+        video['style']['height'] = constraintsArg['video']['height']['ideal'] + 'px';
       }*/
-    } //end if mandatory.video
+    //} //end if constraintsArg.video
 
     WebARRocksMediaStreamAPIHelper.enable_HTMLprop(video, 'autoplay');
     WebARRocksMediaStreamAPIHelper.enable_HTMLprop(video, 'playsinline');
-    const isAudio = (mandatory && mandatory['audio']);
+
+    const isAudio = (constraintsArg && constraintsArg['audio']);
     if (isAudio){
       video['volume'] = 0.0;
     } else {
       WebARRocksMediaStreamAPIHelper.enable_HTMLprop(video, 'muted');
     }
-    console.log('INFO in WebARRocksMediaStreamAPIHelper() - get(): constraints =', JSON.stringify(mandatory));  
+    console.log('INFO in WebARRocksMediaStreamAPIHelper() - get(): constraints =', JSON.stringify(constraintsArg));  
 
-    WebARRocksMediaStreamAPIHelper.get_raw(video, callbackSuccess, function(err){
-      console.log('WARNING in WebARRocksMediaStreamAPIHelper() - get(): cannot request video with this constraints. Downgrade constraints. err=', err);
-      const fallbackConstraints = WebARRocksMediaStreamAPIHelper.create_fallbackConstraints(mandatory);
-      
-      const rec_tryConstraint = function(remainingConstraints){
-        if (remainingConstraints.length === 0){
-          callbackError('INVALID_FALLBACKCONSTRAINTS');
+    WebARRocksMediaStreamAPIHelper.complete_constraintsArg(constraintsArg).then(function(){
+
+      WebARRocksMediaStreamAPIHelper.get_raw(video, callbackSuccess, function(err){
+        console.log('WARNING in WebARRocksMediaStreamAPIHelper() - get(): cannot request video with this constraints. Downgrade constraints. err=', err);
+        const fallbackConstraints = WebARRocksMediaStreamAPIHelper.create_fallbackConstraints(constraintsArg);
+        
+        const rec_tryConstraint = function(remainingConstraints){
+          if (remainingConstraints.length === 0){
+            callbackError('INVALID_FALLBACKCONSTRAINTS');
+            return;
+          }
+          const testedConstraints = remainingConstraints.shift();
+          WebARRocksMediaStreamAPIHelper.get_raw(video, callbackSuccess, function(err){
+            console.log('WARNING: fails with constraints = ', JSON.stringify(testedConstraints), err);
+
+            rec_tryConstraint(remainingConstraints);
+          }, testedConstraints);
+        };
+        rec_tryConstraint(fallbackConstraints);
+      }, constraintsArg);
+    }); // end complete_constraintsArg done
+
+  }, //end WebARRocksMediaStreamAPIHelper.get()
+
+
+  complete_constraintsArg: function(constraintsArg){
+    if (!constraintsArg || !constraintsArg['video'] || !constraintsArg['video']['facingMode']) {
+      return Promise.resolve('NO_VIDEO_CONSTRAINTS');
+    }
+    if (constraintsArg['video']['deviceId']){
+      return Promise.resolve('DEVICEID_ALREADY_SET');
+    }
+    const constraintFacingMode = constraintsArg['video']['facingMode'];
+    const facingMode = constraintFacingMode['exact'] || constraintFacingMode['ideal'];
+    if (!facingMode) {
+      return Promise.resolve('NO_FACINGMODE');
+    }
+    // There is a prefered facing mode. Look for a camera whose name is among a whitelist. If one is found, add its device Id to the constraints
+    const preferredCamerasNamesChunks = {
+      'user': [],
+      'environment': ['camera2 0']
+    }[facingMode];
+    if (!preferredCamerasNamesChunks || preferredCamerasNamesChunks.length === 0){
+      return Promise.resolve('NO_PREFERRED_CAMERAS');
+    }
+
+    return new Promise(function(accept, reject){
+      WebARRocksMediaStreamAPIHelper.get_videoDevices(function(devices){
+        if (!devices){
+          accept('NO_DEVICES_FOUND');
           return;
         }
-        const testedConstraints = remainingConstraints.shift();
-        WebARRocksMediaStreamAPIHelper.get_raw(video, callbackSuccess, function(err){
-          console.log('WARNING: fails with constraints = ', JSON.stringify(testedConstraints), err);
 
-          rec_tryConstraint(remainingConstraints);
-        }, testedConstraints);
-      };
-      rec_tryConstraint(fallbackConstraints);
-    }, mandatory);
-  }, //end WebARRocksMediaStreamAPIHelper.get()
+        const deviceFound = devices.find(function(device){
+          const label = device['label'];
+          if (!label) {
+            return false;
+          }
+          return preferredCamerasNamesChunks.some(function(nameChunk){
+            return label.includes(nameChunk);
+          });
+        });
+
+        if (!deviceFound){
+          accept('NO_PREFERRED_DEVICE_FOUND');
+          return;
+        }
+
+        constraintsArg['video']['deviceId'] = {'exact': deviceFound['deviceId']};
+        accept('SUCCESS');
+      })
+    }); //end returned promise
+  },
   
+
   get_videoDevices: function(callback){
     if (!navigator['mediaDevices'] || !navigator['mediaDevices']['enumerateDevices']) {
       console.log("INFO in WebARRocksMediaStreamAPIHelper - get_videoDevices(): enumerateDevices() not supported");
